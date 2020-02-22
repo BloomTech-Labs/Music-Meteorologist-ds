3e5d7beee2cd0bbad452576716518704a154dc07
Zhenya Warshavsky <zwarshavsky@gmail.com>
2020-02-22 17:50:01 -0500

    updated with canvas 2 genre filtering functionality

--- Flask_AWS/SOUNDDRIP/application.py
+++ Flask_AWS/SOUNDDRIP/application.py
@@ -7,8 +7,7 @@ from flask_cors import CORS
 
 
 
-from models.predict import predictfunc, get_id, get_features, instantiate_sp
-
+from models.predict import instantiate_sp, get_acoustical_features, get_popularity, get_artist_id, get_genres, create_feature_object, get_results, filter_model, song_id_prediction_output, get_user_song_id  
 
 
 
@@ -25,9 +24,26 @@ def prediction():
   content = request.get_json(silent=True)
   token = content["token"]
   sp = instantiate_sp(token)
-  id = get_id(sp)
-  features = get_features(id, sp)
-  return jsonify(predictfunc(features), print('yay'))
+  song_id = get_user_song_id(sp)
+  acoustical_features = get_acoustical_features(song_id, sp)
+  popularity = get_popularity(song_id, sp)
+  song_features_df = create_feature_object(popularity, acoustical_features)
+  results = get_results(song_features_df)
+  source_genre = get_genres(get_artist_id(song_id, sp),sp)
+  filtered_list = filter_model(results,source_genre)
+  return jsonify(song_id_prediction_output(filtered_list)),print('JSON Object Returned')
+
+
+# # Slider endpoint
+# @application.route("/slider", methods=['GET', 'POST'])
+# def prediction():
+#   ''''request flask route takes acoustical features object and outputs the 20 most similar songs'''
+#   content = request.get_json(silent=True)
+#   token = content["token"]
+#   sp = instantiate_sp(token)
+#   id = get_id(sp)
+#   features = get_features(id, sp)
+#   return jsonify(predictfunc(features), print('yay'))
 
 
 @application.route("/")
@@ -36,4 +52,4 @@ def root():
 
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
