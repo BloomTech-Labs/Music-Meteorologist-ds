9edd4252eda9590bf8be1834ba89b5792f45fa5c
Zhenya Warshavsky <zwarshavsky@gmail.com>
2020-02-24 12:18:01 -0500

    Revert "updating flask aws with slider endpoint code"
    This reverts commit baa4d2256ba45a1b806906f30c5e07ca3a3e92a1.

--- Flask_AWS/SOUNDDRIP/application.py
+++ Flask_AWS/SOUNDDRIP/application.py
@@ -1,9 +1,17 @@
-from flask import (Flask, request, jsonify)
+# import spotipy
+# import spotipy.util as util
+from flask import (Flask, render_template, request,
+                   make_response,
+                   jsonify)
 from flask_cors import CORS
-from models.predict import Sound_Drip,Slider
 
 
-# Create Flask app. Should use "application" as variable name for AWS EBS
+
+from models.predict import instantiate_sp, get_acoustical_features, get_popularity, get_artist_id, get_genres, create_feature_object, get_results, filter_model, song_id_prediction_output, get_user_song_id  
+
+
+
+# Create Flask app. Should use "application" as variable name for AWS
 application = Flask(__name__)
 CORS(application)
 
@@ -15,24 +23,27 @@ def prediction():
   ''''request flask route takes token passed in from FE POST and outputs the 20 most similar songs'''
   content = request.get_json(silent=True)
   token = content["token"]
-  SdObj = Sound_Drip(token)
-  song_id,source_genre = SdObj.get_user_song_id_source_genre()
-  acoustical_features = SdObj.get_acoustical_features(song_id)
-  popularity = SdObj.get_popularity(song_id)
-  song_features_df = SdObj.create_feature_object(popularity, acoustical_features)
-  results = SdObj.get_results(song_features_df)
-  filtered_list = SdObj.filter_model(results,source_genre)
-  return jsonify(SdObj.song_id_prediction_output(filtered_list)),print('JSON Object Returned')
+  sp = instantiate_sp(token)
+  song_id = get_user_song_id(sp)
+  acoustical_features = get_acoustical_features(song_id, sp)
+  popularity = get_popularity(song_id, sp)
+  song_features_df = create_feature_object(popularity, acoustical_features)
+  results = get_results(song_features_df)
+  source_genre = get_genres(get_artist_id(song_id, sp),sp)
+  filtered_list = filter_model(results,source_genre)
+  return jsonify(song_id_prediction_output(filtered_list)),print('JSON Object Returned')
 
 
-# Slider endpoint
-@application.route("/slider", methods=['GET', 'POST'])
-def slider_prediction():
-  ''''request flask route takes acoustical features object and outputs the 20 most similar songs'''
-  content = request.get_json(silent=True)
-  slider = Slider(content)
-  slider_predictions = slider.slider_predictions
-  return jsonify(slider_predictions, print('Slider JSON Object Returned'))
+# # Slider endpoint
+# @application.route("/slider", methods=['GET', 'POST'])
+# def prediction():
+#   ''''request flask route takes acoustical features object and outputs the 20 most similar songs'''
+#   content = request.get_json(silent=True)
+#   token = content["token"]
+#   sp = instantiate_sp(token)
+#   id = get_id(sp)
+#   features = get_features(id, sp)
+#   return jsonify(predictfunc(features), print('yay'))
 
 
 @application.route("/")
